85e1080436698a4d3a030d30c1cc1c3683cb31d4
Rihaku <excel.word.outlook@gmail.com>
2024-10-30 23:33:00 +0900

    Update Player.fbx and Player.cs

--- robot_unity/Assets/Player.cs
+++ robot_unity/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -5,12 +6,17 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f; // 移動速度
     public float jumpForce = 5f; // ジャンプ力
     private bool isGrounded; // 地面に接しているかどうか
+    private GameObject playerrig; // PlayerのGameObject
 
     private Rigidbody2D rb; // Rigidbody2D コンポーネント
+
+    //向きを保存
+    private Quaternion player_rotation = Quaternion.Euler(-89.98f, 0, 0);
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // Rigidbody2D コンポーネントを取得
+        playerrig = GameObject.Find("playerrig"); // PlayerのGameObjectを取得
     }
 
     void Update()
@@ -18,11 +24,27 @@ public class Player : MonoBehaviour
         Move(); // 移動処理
         Jump(); // ジャンプ処理
     }
+    void LateUpdate()
+    {
+        float move = Input.GetAxis("Horizontal"); // 水平方向の入力を取得
+
+        // プレイヤーの向きを変える
+        playerrig.transform.rotation = player_rotation;
+    }
     // プレイヤーの移動処理
     void Move()
     {
         float move = Input.GetAxis("Horizontal"); // 水平方向の入力を取得
         rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y); // 速度を設定
+
+        if (move > 0) // 右に移動中
+        {
+            player_rotation = Quaternion.Euler(-89.98f, 90, 0);
+        }
+        else if (move < 0) // 左に移動中
+        {
+            player_rotation = Quaternion.Euler(-89.98f, -90, 0);
+        }
     }
 
     // プレイヤーのジャンプ処理
